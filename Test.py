b97e35449ab201f405ff4109000e067863302121
Sudhanshu <66222544+SSudhashu@users.noreply.github.com>
2021-01-15 19:53:50 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,19 +1,44 @@
-from flask import Flask,request,render_template
+# using flask_restful
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
 
-
-
+# creating the flask app
 app = Flask(__name__)
-
-@app.route('/')
-
-def home():
-    return "Hello!! <h1> Testing<h1>"
-
-@app.route('/ok/<name>')
-
-def greet(name):
-    return "<br><h2>Hello Mr/Miss {}".format(name)
+# creating an API object
+api = Api(app)
 
 
+# making a class for a particular resource
+# the get, post methods correspond to get and post requests
+# they are automatically mapped by flask_restful.
+# other methods include put, delete, etc.
+class Hello(Resource):
+
+    # corresponds to the GET request.
+    # this function is called whenever there
+    # is a GET request for this resource
+    def get(self):
+        return jsonify({'message': 'hello world'})
+
+        # Corresponds to POST request
+
+    def post(self):
+        data = request.get_json()  # status code
+        return jsonify({'data': data}), 201
+
+
+# another resource to calculate the square of a number
+class Square(Resource):
+
+    def get(self, num):
+        return jsonify({'square': num ** 2})
+
+    # adding the defined resources along with their corresponding urls
+
+
+api.add_resource(Hello, '/')
+api.add_resource(Square, '/square/<int:num>')
+
+# driver function
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug=True)
